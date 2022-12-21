a03af15951390eae9436526cfa5b6162c27d4ac9
Frentyusmelia <114353586+Frentyusmelia@users.noreply.github.com>
2022-12-21 18:23:03 +0700

    Add files via upload

---
+++ Frentyusmelia151222/EntryBukuAlamat.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Frentyusmelia151222;
+
+import Frentyusmelia151222.BukuAlamat;
+
+/**
+ *
+ * @author ACER
+ */
+public class EntryBukuAlamat {
+    public static void main(String[] args) {
+        BukuAlamat BukuAlamat1 = new BukuAlamat();
+        
+        BukuAlamat1.setNama("Frentyusmelia");
+        BukuAlamat1.setAlamat("Kapalo Koto");
+        BukuAlamat1.setTelepon("082171720916");
+        BukuAlamat1.setEmail("frenty100420@gmail.com");
+        
+        BukuAlamat1.print();
+    }
+}
