8934291cd0a378fcac36307614b82b1eb68d16c0
Frentyusmelia <114353586+Frentyusmelia@users.noreply.github.com>
2022-12-29 12:31:39 +0700

    Add files via upload

--- Frentyusmelia151222_Modul11/Student.java
+++ Frentyusmelia151222_Modul11/Student.java
@@ -10,19 +10,32 @@ package Frentyusmelia151222_Modul11;
  * @author ACER
  */
 public class Student extends Person {
+    
+    private String nobp;
+    
     public Student(){ 
         super("Anna","Padang");
         System.out.println("Inside Student:Constructor"); 
     } 
     
+    @Override
     public String getName(){ 
         System.out.println("get Name Student");
         return name; 
     }
- 
+    
+    public String getNobp(){ 
+        System.out.println("get Name Student");
+        return nobp; 
+    }
+    
     public static void main( String[] args ){ 
         Student anna = new Student();
+//        anna.name ="Anna";
+//        anna.address ="Padang";
+//        anna.nobp ="1111111";
         System.out.println(anna.name);
-        System.out.println(anna.address);
+//        System.out.println(anna.address);
+//        System.out.println(anna.nobp);
     }
 }
